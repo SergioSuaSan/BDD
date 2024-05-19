846d22ae47f902cd7363c7ce145104e8fe9308b5
SergioSuaSan <sergiosuarez95@yahoo.es>
2024-05-19 18:13:05 +0200

    jaj

--- 3Trimestre/Mayo 18/tema3_dam~3.sql
+++ 3Trimestre/Mayo 18/tema3_dam~3.sql
@@ -2,10 +2,22 @@ declare
     v_kk number;
 
 BEGIN
-    EXECUTE IMMEDIATE 'DROP TABLE KK';
+    begin 
+        EXECUTE IMMEDIATE 'DROP TABLE KK';
+    exception
+        when others then
+        dbms_output.put_line('kok');
+    end;
+    
+    begin
+   
     EXECUTE IMMEDIATE 'CREATE TABLE KK (COL1 number(4))';
+    end;
+   
     EXECUTE IMMEDIATE 'insert into kk values (345)';
-    execute immediate 'select count(*) into V_kk from kk';
+    select count(*) into v_kk from kk;
+    execute immediate 'select count(*) from kk' into v_kk;
+    dbms_output.put_line(v_kk);
     
     
         
