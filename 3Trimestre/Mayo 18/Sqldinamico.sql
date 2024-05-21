15af4e357e2fee82af258bddae3b78c2669a9e1b
SergioSuaSan <148342257+SergioSuaSan@users.noreply.github.com>
2024-05-21 09:08:36 +0200

    21/5

--- 3Trimestre/Mayo 18/Sqldinamico.sql
+++ 3Trimestre/Mayo 18/Sqldinamico.sql
@@ -1,17 +1,22 @@
 DECLARE
     v_num number;
+    v_tabla varchar (23);
 Begin
+    v_tabla:= 'torito22';
     begin 
-        Execute immediate 'drop table kk';
+        Execute immediate 'drop table '|| v_tabla;
+    exception
+    when others then
+     dbms_output.put_line('No se pudo borrar');
     end;
     
     Begin
-        Execute immediate 'create table kk (col1 number(3))';
-        Execute immediate ' insert into kk values (3)';
+        Execute immediate 'create table '||v_tabla||' (col1 number(3))';
+        Execute immediate ' insert into '||v_tabla||' values (3)';
     end;
     
     Begin
-        Execute immediate ' select count(*) from kk' into v_num;
+        Execute immediate ' select count(*) from '||v_tabla into v_num;
     end;
     
     dbms_output.put_line(v_num);
